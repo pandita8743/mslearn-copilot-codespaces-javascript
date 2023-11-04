054f731695f1a954daf84345b30f26a529706896
Pandita <146020318+pandita8743@users.noreply.github.com>
2023-11-04 21:26:40 +0000

    Creación Base de Portafolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,10 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Roy Chavarría Garita",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  email: "roychavarriagarita@gmail.com",
+  gitHub: "pandita8743",
   instagram: "microsoft",
   linkedIn: "satyanadella",
   medium: "",
